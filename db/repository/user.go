b87fef11468038f6d280daec586e0da4b0948efd
Nick Gowdy <nickgowdy87@gmail.com>
2022-10-02 10:00:35 +0100

    Get user by id query

---
+++ db/repository/user.go
@@ -0,0 +1,41 @@
+package repository
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/recipe-api/m/models"
+)
+
+func GetUser(id int) (returnedUser models.User, err error) {
+
+	var psqlconn = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("host"), os.Getenv("port"), os.Getenv("user"), os.Getenv("password"), os.Getenv("dbname"))
+
+	db, err := sql.Open("postgres", psqlconn)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	rows, err := db.Query("select id, name from users where id = ?", id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&returnedUser)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(id)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return returnedUser, nil
+}
