0e696d7c419a8595be0f0047985230e098b43303
Nick Gowdy <nickgowdy87@gmail.com>
2023-02-06 17:05:32 +0000

    Tidy up

--- auth/auth.claims.go
+++ auth/auth.claims.go
@@ -14,7 +14,6 @@ func GetClaimsFromToken(r *http.Request) (jwt.MapClaims, error) {
 	authHeader := strings.Split(r.Header.Get("Authorization"), "Bearer ")
 	if len(authHeader) != 2 {
 		return nil, fmt.Errorf("http.StatusUnauthorized")
-		// w.WriteHeader(http.StatusUnauthorized)
 	} else {
 		tokenString := authHeader[1]
 		_, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
