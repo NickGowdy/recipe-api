65572cbacefa60ea36dd9badda60942ddd00aab4
Nick Gowdy <nickgowdy@Nicks-MacBook-Pro.local>
2023-08-02 13:05:21 +0100

    tidy up main.go

--- main.go
+++ main.go
@@ -11,27 +11,32 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/recipe-api/database"
 	"github.com/recipe-api/handlers"
-	"github.com/recipe-api/recipeDb"
 	"github.com/recipe-api/repository"
 )
 
-func main() {
-	godotenv.Load()
+const (
+	driver  = "postgres"
+	timeout = 30
+)
 
-	recipeDb.Migrate()
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		os.Getenv("host"), os.Getenv("port"), os.Getenv("user"), os.Getenv("password"), os.Getenv("dbname"))
 
-	db, err := sql.Open("postgres", psqlconn)
+	db, err := sql.Open(driver, psqlconn)
 
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
 	}
 
 	queries := database.New(db)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), timeout*time.Second)
 	defer cancel()
 
 	repo := repository.NewRecipeRepository(queries, &ctx)
