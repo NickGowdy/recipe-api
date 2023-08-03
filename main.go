4e598fc91e1d85d728785720ca6b2fd224f6d74b
Nick Gowdy <nickgowdy@Nicks-MacBook-Pro.local>
2023-08-03 14:10:24 +0100

    working with docker

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"github.com/recipe-api/database"
 	"github.com/recipe-api/handlers"
@@ -30,7 +32,7 @@ func main() {
 	}
 
 	psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("host"), os.Getenv("port"), os.Getenv("user"), os.Getenv("password"), os.Getenv("dbname"))
+		os.Getenv("host"), os.Getenv("dbport"), os.Getenv("user"), os.Getenv("password"), os.Getenv("dbname"))
 
 	db, err := sql.Open(driver, psqlconn)
 
@@ -53,10 +55,42 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), seconds*time.Second)
 	defer cancel()
 
+	go func() {
+		select {
+		case <-time.After(seconds * time.Second):
+			fmt.Println("overslept")
+		case <-ctx.Done():
+			fmt.Println(ctx.Err()) // prints "context deadline exceeded"
+		}
+	}()
+
 	userRepository := repository.NewUserRepository(queries, &ctx)
 	recipeRepository := repository.NewRecipeRepository(queries, &ctx)
 
-	routes := handlers.NewHandlers(&userRepository, &recipeRepository)
+	log.Println("Loading routes...")
+	mr := mux.NewRouter()
 
-	routes.SetupRoutes()
+	mr.HandleFunc("/register", handlers.PostRegisterHandler(&userRepository)).Methods("POST")
+	mr.HandleFunc("/login", handlers.PostLoginHandler(&userRepository)).Methods("POST")
+
+	mr.Handle("/recipe", handlers.Middleware(handlers.GetAllRecipesHandler(&recipeRepository))).Methods("GET")
+	mr.Handle("/recipe/{id}", handlers.Middleware(handlers.GetRecipeHandler(&recipeRepository))).Methods("GET")
+	mr.Handle("/recipe", handlers.Middleware(handlers.InsertRecipeHandler(&recipeRepository))).Methods("POST")
+	mr.Handle("/recipe/{id}", handlers.Middleware(handlers.UpdateRecipeHandler(&recipeRepository))).Methods("PUT")
+	mr.Handle("/recipe/{id}", handlers.Middleware(handlers.DeleteRecipeHandler(&recipeRepository))).Methods("DELETE")
+
+	mr.HandleFunc("/health-check", HealthCheck).Methods("GET")
+
+	http.Handle("api/", mr)
+	serverPort := fmt.Sprintf(":%s", os.Getenv("serverport"))
+	err = http.ListenAndServe(serverPort, mr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("API is now up...")
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "API is up and running")
 }
